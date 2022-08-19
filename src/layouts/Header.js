23eee7feed3823fdfdd5196886cfc6dbcba3358b
Candice Ariel <candicewangsa@gmail.com>
2022-08-19 21:34:55 +0700

    Header update

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -5,12 +5,21 @@ import "../assets/scss/main.scss";
 const Header = () => {
   return (
     <div className="header">
-        <h1 className="header_logo">Mangaten</h1>
-        <ul className= "nav_header">
-            <li className= "nav_header-item">My account</li>
-            <li className= "nav_header-item">Wishlist</li>
-            <li className= "nav_header-item">Cart</li>
+      <div className="header-top">
+        <ul className= "nav_header-top text-xs">
+          <li className= "nav_header-item">My account</li>
+          <li className= "nav_header-item">Wishlist</li>
+          <li className= "nav_header-item">Cart</li>
         </ul>
+      </div>
+      <div className="header-bottom">
+        <h1 className="header_logo">Mangaten</h1>
+        <ul className= "nav_header-bottom">
+          <li className= "nav_header-item">My account</li>
+          <li className= "nav_header-item">Wishlist</li>
+          <li className= "nav_header-item">Cart</li>
+        </ul>
+      </div>
     </div>
   )
 }
