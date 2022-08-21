55f1a874011939ce092a6e42886061156a76535a
Candice Ariel <candicewangsa@gmail.com>
2022-08-21 19:58:30 +0700

    Seperate data file

--- src/components/BooksShelf.js
+++ src/components/BooksShelf.js
@@ -1,38 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import { books } from "../data/books";
 
 function BooksShelf() {
   const shelves = [
     {
         shelvesId: "NR",
         label: "New Release",
-        products: [
-            {
-                productId: "M001",
-                title: "Tokyo Revengers",
-                price: 4.00
-            },
-            {
-                productId: "M002",
-                title: "Your Name",
-                price: 4.00
-            },
-            {
-                productId: "M002",
-                title: "Your Name",
-                price: 4.00
-            },
-            {
-                productId: "M002",
-                title: "Your Name",
-                price: 4.00
-            }
-        ]
+        prod: {books}
     },
     {
         shelvesId: "BS",
-        label: "Best Seller"
+        label: "Best Seller",
+        prod: {books}
     }
   ]
+
+  console.log(shelves)
+
 
   return (
     <div>
@@ -40,14 +25,14 @@ function BooksShelf() {
             <div key={shelf.shelvesId}>
                 <h4 key={shelf.shelvesId} className= "shelf-title">{shelf.label}</h4>
                 <ul className= "list_products list-flex-view">
-                    {shelf.products?.map(product => 
+                    {shelf.prod.books?.map(product => (
                         <div className='box-small'>
                             <li className= "list_nav_item list-inline" key={product.productId}>
                                 <p>{product.title}</p>
                                 <p className="text_product-price">${product.price}</p>
                             </li>   
                         </div>
-                    )}
+                    ))}
                 </ul>
             </div>
         )}
---
+++ src/data/books.js
@@ -0,0 +1,22 @@
+export const books= [
+    {
+        productId: "M001",
+        title: "Tokyo Revengers",
+        price: 4.00
+    },
+    {
+        productId: "M002",
+        title: "Your Name",
+        price: 4.00
+    },
+    {
+        productId: "M002",
+        title: "Your Name",
+        price: 4.00
+    },
+    {
+        productId: "M002",
+        title: "Your Name",
+        price: 4.00
+    }
+];
