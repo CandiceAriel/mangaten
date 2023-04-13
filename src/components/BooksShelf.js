ddddf1242dbf95b88c4e9bf742d63c61d4ade387
Candice Ariel <candicewangsa@gmail.com>
2023-04-13 20:30:49 +0700

    change book list to NavLink

--- src/components/BooksShelf.js
+++ src/components/BooksShelf.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { NavLink } from "react-router-dom";
 
 import { books, bookCategories } from "../data/data";
 
@@ -8,16 +9,25 @@ function BooksShelf() {
     <div className="c-bookshelf">
       {bookCategories.map(category =>
         <div key={category.shelvesId} className="c-bookshelf__shelves">
-          <h4 key={category.shelvesId} className="c-bookshelf__shelf-title">{category.categoryName}</h4>
-          <div key={category.shelvesId} className="c-bookshelf__shelf">
-            {books?.map(product => (
-              <div className='c-bookshelf__shelf-books box_product box-small' key={product.productId}>
-                <div className="list_nav_item list-inline">
-                  <img src={product.src}></img>
-                  <p>{product.title}</p>
-                  <p className="text_product-price">${product.price}</p>
-                </div>
-              </div>
+          <h4 className="c-bookshelf__shelf-title">{category.categoryName}</h4>
+          <div className="c-bookshelf__shelf">
+            {category.items?.map((product) => (
+              // <div className='c-bookshelf__shelf-books box_product box-small' key={product.productId}>
+              //   <div className="list_nav_item list-inline">
+              //     <img src={product.src}></img>
+              //     <p>{product.title}</p>
+              //     <p className="text_product-price">${product.price}</p>
+              //   </div>
+              // </div>
+              <NavLink
+              to={`/${product.title}`}
+              key={product.productId}
+              className='c-bookshelf__shelf-books box_product box-small'
+            >
+              <img src={product.src}></img>
+              <p>{product.title}</p>
+              <p className="text_product-price">${product.price}</p>
+            </NavLink>
             ))}
           </div>
         </div>
--- src/data/data.js
+++ src/data/data.js
@@ -1,48 +1,61 @@
 import TokyoRevengersCover from "../assets/images/TokyoRevengersCover.jpg";
 
-export const books= [
-    {
+export const books = [
+  {
+    productId: "M001",
+    src: TokyoRevengersCover,
+    title: "Tokyo Revengers",
+    price: 4.00
+  },
+  {
+    productId: "M002",
+    title: "Your Name",
+    price: 4.00
+  },
+  {
+    productId: "M003",
+    title: "Your Name",
+    price: 4.00
+  },
+  {
+    productId: "M004",
+    title: "Your Name",
+    price: 4.00
+  }
+];
+
+export const bookCategories = [
+  {
+    categoryId: "C01",
+    categoryName: "Manga",
+    items: [
+      {
         productId: "M001",
         src: TokyoRevengersCover,
         title: "Tokyo Revengers",
         price: 4.00
-    },
-    {
+      },
+      {
         productId: "M002",
         title: "Your Name",
         price: 4.00
-    },
-    {
-        productId: "M003",
-        title: "Your Name",
-        price: 4.00
-    },
-    {
-        productId: "M004",
-        title: "Your Name",
-        price: 4.00
-    }
-];
-
-export const bookCategories = [
-    {
-      categoryId: "C01",
-      categoryName: "Manga"
-    },
-    {
-      categoryId: "C02",
-      categoryName: "Novel"
-    },
-    {
-      categoryId: "C03",
-      categoryName: "Artbook"
-    },
-    {
-      categoryId: "C04",
-      categoryName: "Study Guide"
-    },
-    {
-      categoryId: "C05",
-      categoryName: "Others"
-    }
+      },
+    ],
+  },
+  {
+    categoryId: "C02",
+    categoryName: "Novel"
+  },
+  {
+    categoryId: "C03",
+    categoryName: "Artbook"
+  },
+  {
+    categoryId: "C04",
+    categoryName: "Study Guide"
+  },
+  {
+    categoryId: "C05",
+    categoryName: "Others"
+  }
 ]
