3e6905d0be912773e6f0f718374d635d355fd2e3
Candice Ariel <candicewangsa@gmail.com>
2023-04-21 22:04:45 +0700

    format number to currency

--- client/src/components/BooksShelf.jsx
+++ client/src/components/BooksShelf.jsx
@@ -32,18 +32,15 @@ function BooksShelf() {
       });
   };
 
-  function makeDecimal(num){
+  // Format the price above to USD using the locale, style, and currency.
+  let USDollar = new Intl.NumberFormat('en-US', {
+    maximumFractionDigits: 2, 
+    minimumFractionDigits: 2
+  });
 
-    var leftDecimal = num.toString().replace('.', ''),
-        rightDecimal = '00';
-    
-    if(leftDecimal.length > 2){          
-      rightDecimal = leftDecimal.slice(-2);
-      leftDecimal = leftDecimal.slice(0, -2);
-    }
-    
-    var n = Number(leftDecimal+'.'+rightDecimal).toFixed(2);        
-    return (n === "NaN") ? num:n        
+  function makeDecimal(price){
+    var num = USDollar.format(price)
+    return num;
   }
 
   useEffect(() => {
@@ -84,7 +81,7 @@ function BooksShelf() {
               )) 
             } 
            { category.categoryName === "Artbook" && 
-              books.filter(item => item.categoryId === "C03").map((item) => (
+              result.filter(item => item.categoryId === "C03").map((item) => (
                 <NavLink
                   to={`/${item.title}`}
                   key={item.productId}
@@ -92,7 +89,7 @@ function BooksShelf() {
                 >
                   <img src={item.src}></img>
                   <p className="c-base__text_bold">{item.title}</p>
-                  <p className="text_product-price">${item.price}</p>
+                  <p className="text_product-price">${makeDecimal(item.price)}</p>
                 </NavLink>  
               )) 
             } 
