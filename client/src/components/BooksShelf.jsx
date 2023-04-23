292fa1ae5b83b1108f44dc53a3242437d643b1e1
Candice Ariel <candicewangsa@gmail.com>
2023-04-23 16:23:07 +0700

    Item wrapper for pos

--- client/src/components/BooksShelf.jsx
+++ client/src/components/BooksShelf.jsx
@@ -2,7 +2,6 @@ import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import { NavLink } from "react-router-dom";
 
-
 import { books, bookCategories } from "../data/booksData";
 
 function BooksShelf() {
--- client/src/pages/Pos.jsx
+++ client/src/pages/Pos.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios';
+
 import MainNavigation from '../components/MainNavigation';
 
 const Pos = () => {
+  const [result, setResult] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  function fetchBoook() {
+    axios.get('http://localhost:3001/api/books')
+      .then(response => {
+        const data = response.data;
+        setResult(data);
+        console.log(data)
+      })
+      .catch(() => {
+        alert('Error retrieving data!!!');
+      });
+  };
+
+  // Format the price above to USD using the locale, style, and currency.
+  let USDollar = new Intl.NumberFormat('en-US', {
+    maximumFractionDigits: 2, 
+    minimumFractionDigits: 2
+  });
+
+  function makeDecimal(price){
+    var num = USDollar.format(price)
+    return num;
+  }
+
+  useEffect(() => {
+    fetchBoook();
+  }, []);
+
   return (
     <div className='c-pos'>
       <MainNavigation isPOS={true} />
       <div className='c-pos__wrapper'>
         <div className='c-pos__wrapper-items'>
-          Items
+          { result.map((item, i) => (
+            <div
+              key={item.productId}
+              className='c-pos__item'
+            >
+              <img src={item.src}></img>
+              <p className="c-base__text_bold">{item.title}</p>
+              <p className="text_product-price">${makeDecimal(item.price)}</p>
+            </div>  
+            )) 
+          }
         </div>
         <div className='c-pos__wrapper-receipt'>
           Receipt
